d56fde50b849f33385202adb11926bf7d278286a
robertgruevski <robertgruevski@outlook.com>
2025-09-03 22:58:24 -0400

    API - Add Blog Post Functionality (HTTP POST)

---
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,52 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BlogPostsController : ControllerBase
+	{
+		private readonly IBlogPostRepository blogPostRepository;
+
+		public BlogPostsController(IBlogPostRepository blogPostRepository)
+		{
+			this.blogPostRepository = blogPostRepository;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+		{
+			var blogPost = new BlogPost()
+			{
+				Title = request.Title,
+				ShortDescription = request.ShortDescription,
+				Content = request.Content,
+				FeatureImageUrl = request.FeatureImageUrl,
+				UrlHandle = request.UrlHandle,
+				PublishedDate = request.PublishedDate,
+				Author = request.Author,
+				IsVisible = request.IsVisible
+			};
+			blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+			var response = new BlogPostDto()
+			{
+				Id = blogPost.Id,
+				Title = blogPost.Title,
+				ShortDescription = blogPost.ShortDescription,
+				Content = blogPost.Content,
+				FeatureImageUrl = blogPost.FeatureImageUrl,
+				UrlHandle = blogPost.UrlHandle,
+				PublishedDate = blogPost.PublishedDate,
+				Author = blogPost.Author,
+				IsVisible = blogPost.IsVisible
+			};
+
+			return Ok(response);
+		}
+	}
+}
---
+++ CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+	public class BlogPostRepository : IBlogPostRepository
+	{
+		private readonly ApplicationDbContext context;
+
+		public BlogPostRepository(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+		{
+			await context.BlogPosts.AddAsync(blogPost);
+			await context.SaveChangesAsync();
+			return blogPost;
+		}
+	}
+}
