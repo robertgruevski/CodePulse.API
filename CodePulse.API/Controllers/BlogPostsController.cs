3da6ea9c65553347ac1d30a37f6662d250c5c1e8
robertgruevski <robertgruevski@outlook.com>
2025-09-05 16:55:30 -0400

    API - GET BlogDetails by URL

--- CodePulse.API/Controllers/BlogPostsController.cs
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -130,6 +130,38 @@ namespace CodePulse.API.Controllers
 			return Ok(response);
 		}
 
+		[HttpGet("{urlHandle}")]
+		public async Task<IActionResult> GetBlogPostByUrlHandle([FromRoute] string urlHandle)
+		{
+			var blogPost = await blogPostRepository.GetByUrlHandleAsync(urlHandle);
+
+			if (blogPost is null)
+			{
+				return NotFound();
+			}
+
+			var response = new BlogPostDto()
+			{
+				Id = blogPost.Id,
+				Title = blogPost.Title,
+				ShortDescription = blogPost.ShortDescription,
+				Content = blogPost.Content,
+				FeaturedImageUrl = blogPost.FeaturedImageUrl,
+				UrlHandle = blogPost.UrlHandle,
+				PublishedDate = blogPost.PublishedDate,
+				Author = blogPost.Author,
+				IsVisible = blogPost.IsVisible,
+				Categories = blogPost.Categories.Select(x => new CategoryDto
+				{
+					Id = x.Id,
+					Name = x.Name,
+					UrlHandle = x.UrlHandle
+				}).ToList()
+			};
+
+			return Ok(response);
+		}
+
 		[HttpPut("{id:guid}")]
 		public async Task<IActionResult> UpdateBlogPostById([FromRoute] Guid id, [FromBody] UpdateBlogPostRequestDto request)
 		{
